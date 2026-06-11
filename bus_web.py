bc715a64e2937a9dbe1b001845e245060a4ce069
Luke0515-luke <0515luke@gmail.com>
2026-06-11 23:03:33 +0800

    Update bus_web.py

--- bus_web.py
+++ bus_web.py
@@ -312,8 +312,12 @@ if __name__ == '__main__':
                         progress_bar = st.progress(0)
                         
                                             # 1. 確保有這行！初始化 HTML 字串容器
-                        html_buffer = '<div class="timeline-container">'
-                        ai_log_list = []
+                                            html_buffer = '<div class="timeline-container">'
+                    ai_log_list = []
+                    
+                    for item in active_list:
+                        s_name = item.get("StopName", {}).get("Zh_tw", "未知站點")
+
                     
                     
 
